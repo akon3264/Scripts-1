b655e17800ba3abbc5641f2b6147e5540f6d97b4
koolkaracter <koolkaracter123@gmail.com>
2014-09-22 17:58:39 -0500

    Update DariusIsKool.lua

--- Champs/DariusIsKool.lua
+++ Champs/DariusIsKool.lua
@@ -1,3 +1,17 @@
+-- ************************** LBC META *****************************
+-- * lbc_name = SwainIsKool.lua
+-- * lbc_version = 1.1
+-- * lbc_date = 09/07/2014 // use correct date format mm/dd/yyyy
+-- * lbc_status = 3 // 0 = unknowen; 1 = alpha/wip; 2 = beta; 3 = ready; 4 = required; 5 = outdated
+-- * lbc_type = 3 // 0 = others; 1 = binaries; 2 = libs; 3 = champion; 4 = hotkey; 5 = utility
+-- * lbc_creator = KoolKaracter
+-- * lbc_champion = Darius // if this script is for a special champ
+-- * lbc_tags = Darius, dunk, demacia, AD, Kool, kool, iskool, koolkaracter
+-- * lbc_link = http://leaguebot.net/forum/Upload/showthread.php?tid=4316
+-- * lbc_source = https://raw.githubusercontent.com/koolkaracter/Scripts/AutoKool/Champs/DariusIsKool.lua
+-- * lbc_update = // only if you have a new version on a new source
+-- ************************** LBC META *****************************
+
 local ScriptName = 'DariusIsKool'									
 local Version = '1.1'												
 local Author = 'Koolkaracter'												
